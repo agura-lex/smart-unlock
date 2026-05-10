75aacbdd92873134a5aca485a7ed04edb7affc7e
Alexander Agura <agura.alex@gmail.com>
2026-05-10 20:09:07 +0300

    fix debug when 0 and add notifications

--- smart-unlock.sh
+++ smart-unlock.sh
@@ -8,8 +8,11 @@ readonly RUNDIR="$XDG_RUNTIME_DIR/smart-unlock" # TODO: sanity checks for this
 readonly MODDIR='./modules' # TODO: adapt to system-wide installation
 
 err(){ echo $@ 1>&2; }
+
 debug(){
-    [ "$DEBUG" == 1 ] && echo 'debug:' "$@"
+    if [ "$DEBUG" == 1 ]; then
+        echo 'debug:' "$@"
+    fi
 }
 
 if [ -f "$CONFIG_FILE" ]; then
@@ -21,17 +24,27 @@ fi
 
 mod(){ # Module action wrapper
     # TODO: add sanity checks
-    mod_$MOD "$@" 2>&1 | sed "s/^/$MOD: /"
+    mod_$MOD "$@" 2>&1
 }
+
+mod_log(){ # Module action wrapper, prepend module name
+    # TODO: add sanity checks
+    mod "$@" 2>&1 | sed "s/^/$MOD: /"
+}
+
+
 
 unlock_if_locked(){
     if loginctl show-session $XDG_SESSION_ID -p LockedHint |
         awk -F= '{print $2}' | grep -q '^yes$'
     then
         echo "Session locked, unlocking" >&2
-        loginctl unlock-session $XDG_SESSION_ID
-        #notify-send -i unlock -a $0\
-            #"Session unlocked due to \"$DEVICE_NAME\" connecting"
+        loginctl unlock-session "$XDG_SESSION_ID"
+        if [ "$NOTIFY" ]; then
+            DEV_NAME="$(mod pretty_name)"
+            notify-send -i unlock -a 'Smart Unlock'\
+                "Session unlocked due to \"$DEV_NAME\" connecting via \"$MOD\""
+        fi
     else
         debug "Session unlocked already, not doing anything"
     fi
@@ -41,10 +54,12 @@ unlock_if_locked(){
 mkdir -vpm 700 "$RUNDIR"
 
 # initialize modules
+# TODO: check deps
 for MOD in "${MODULES[@]}"; do
     echo "Initializing module '$MOD'..."
     . "$MODDIR/$MOD"
-    mod init
+    mod_log init
+    debug "MOD_DESC='$MOD_DESC'"
 done
 unset MOD
 
@@ -55,7 +70,7 @@ echo "Initializing device $DEV_STR..."
     DEV_ID=$(awk -F '::' '{ print $2}' <<<"$DEV_STR")
     debug "Device ID: $DEV_ID, module: $MOD"
 
-    mod init_dev $DEV_ID
+    mod_log init_dev $DEV_ID
 done
 unset DEV
 unset MOD
@@ -66,7 +81,7 @@ while true; do
     for DEV_STR in "${DEVICES[@]}"; do
         MOD=$(awk -F '::' '{ print $1}' <<<"$DEV_STR")
         DEV_ID=$(awk -F '::' '{ print $2}' <<<"$DEV_STR")
-        if mod check_connect "$DEV_ID"; then
+        if mod_log check_connect "$DEV_ID"; then
             unlock_if_locked
         fi
     done
